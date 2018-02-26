6bf3b6004c2c3312d0244b9c09e3b5c13670e0f1
Dan Kee <dan@dankee.com>
2018-02-26 00:30:14 +0000

    Add save hook

---
+++ sciencebox/post_save_config.py
@@ -0,0 +1,11 @@
+import os
+from subprocess import check_call
+
+def post_save(model, os_path, contents_manager):
+    """post-save hook for converting notebooks to .py scripts"""
+    if model['type'] != 'notebook':
+        return # only do this for notebooks
+    d, fname = os.path.split(os_path)
+    check_call(['jupyter', 'nbconvert', '--to', 'python', fname], cwd=d)
+
+c.FileContentsManager.post_save_hook = post_save
